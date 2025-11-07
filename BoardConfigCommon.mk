dd1916fd6dc119401a29777578e9cf839abb4d08
iHSAN <ihsanulrahman@proton.me>
2025-11-07 15:15:27 +0500

    test
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,7 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 	hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
 	$(COMMON_PATH)/configs/vintf/device_framework_matrix.xml \
+	hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
 	vendor/arrow/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
